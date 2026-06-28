6f7345c415b57ee0db83b54b1b9dcc2c0b348753
Bohdan Katsenko <bohdan_katsenko@epam.com>
2026-06-28 05:14:39 +0300

    wrote a reminder comment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// TODO: write tests for this function
 export function generateRandomInteger(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
